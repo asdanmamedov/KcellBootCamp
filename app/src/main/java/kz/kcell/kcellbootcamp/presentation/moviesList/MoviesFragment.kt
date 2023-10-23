50eccd6910a8ab3ad0a461b564e0be95310482e3
Assasin321 <astanmamedov@gmail.com>
2023-10-24 00:20:59 +0600

    2 задание

--- app/src/main/java/kz/kcell/kcellbootcamp/presentation/moviesList/MoviesFragment.kt
+++ app/src/main/java/kz/kcell/kcellbootcamp/presentation/moviesList/MoviesFragment.kt
@@ -6,6 +6,7 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kz.kcell.kcellbootcamp.R
@@ -27,7 +28,10 @@ class MoviesFragment : Fragment(R.layout.fragment_movies) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.movieRecyclerView.adapter = this@MoviesFragment.adapter
+        val layoutManager = LinearLayoutManager(context)
+        binding.movieRecyclerView.layoutManager = layoutManager
         setupObservers()
+
     }
 
     private fun setupObservers() {
